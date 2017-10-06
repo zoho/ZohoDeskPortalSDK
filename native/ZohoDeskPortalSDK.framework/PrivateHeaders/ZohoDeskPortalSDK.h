4c41834dcef1b62433ff720fb50210ee61aeae28
rajeshkumar.l <rajeshkumar.l@zohocorp.com>
2017-10-06 13:14:54 +0530

    Enhancement

---
+++ native/ZohoDeskPortalSDK.framework/PrivateHeaders/ZohoDeskPortalSDK.h
@@ -0,0 +1,39 @@
+//
+//  ZohoDeskSDK.h
+//  ZohoDeskSDK
+//
+//  Created by rajeshkumar.l on 07/10/16.
+//  Copyright © 2016 rajesh-2098. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZohoDeskSDK.
+FOUNDATION_EXPORT double ZohoDeskPortalSDKVersionNumber;
+
+//! Project version string for ZohoDeskSDK.
+FOUNDATION_EXPORT const unsigned char ZohoDeskPortalSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZohoDeskPortalSDK/PublicHeader.h>
+
+
+#import <ZohoDeskPortalSDK/LCReachability.h>
+#import <ZohoDeskPortalSDK/PEXHeader.h>
+#import <ZohoDeskPortalSDK/WmsConfig.h>
+#import <ZohoDeskPortalSDK/MessageHandler.h>
+#import <ZohoDeskPortalSDK/LDPexAdaptor.h>
+#import <ZohoDeskPortalSDK/LDPexHeader.h>
+#import <ZohoDeskPortalSDK/Chathandler.h>
+#import <ZohoDeskPortalSDK/SwiftTryCatch.h>
+#import <ZohoDeskPortalSDK/asl.h>
+#import <ZohoDeskPortalSDK/ApiHeaders.h>
+//#import <ZohoDeskSDK/ClinetEnums.h>
+
+
+#import <ZohoDeskPortalSDK/ClientZIAMUtil.h>
+//#import <ZohoDeskSDK/ApiHeaders.h>
+//#import <ZohoDeskSDK/ApiHeaders.h>
+//#import <ZohoDeskSDK/ApiHeaders.h>
+//#import <ZohoDeskSDK/ApiHeaders.h>
+//#import <ZohoDeskSDK/ApiHeaders.h>
+
